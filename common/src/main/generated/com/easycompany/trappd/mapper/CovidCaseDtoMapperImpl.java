37c4f842967eeb7a4d555dbc83b892c0b6c56724
princebansal <prince.bansal@traveloka.com>
2020-04-13 04:04:37 +0530

    Fixed test case

--- common/src/main/generated/com/easycompany/trappd/mapper/CovidCaseDtoMapperImpl.java
+++ common/src/main/generated/com/easycompany/trappd/mapper/CovidCaseDtoMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-13T02:14:36+0530",
+    date = "2020-04-13T04:02:38+0530",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_242 (Oracle Corporation)"
 )
 @Component
--- data-service/src/test/java/com/easycompany/trappd/service/DataUpdaterServiceTest.java
+++ data-service/src/test/java/com/easycompany/trappd/service/DataUpdaterServiceTest.java
@@ -15,9 +15,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@Transactional
 class DataUpdaterServiceTest {
 
   @Autowired DataUpdaterService dataUpdaterService;
